b501a1ab57e8d002c1853448a9e08535e7d20350
chrisdeckard2 <christopherdaviddeckard@gmail.com>
2024-06-19 10:19:31 -0400

    Add project files.

---
+++ Library/Program.cs
@@ -0,0 +1,17 @@
+﻿using MyLibrary;
+
+namespace Library;
+
+internal class Program {
+
+    static void Main(string[] args) {
+
+        Console.WriteLine(Class1.About);
+
+        var c1 = new Class1();
+
+        Console.WriteLine(c1.About2);
+
+
+    }
+}
